633ba09b66d8f289f6266dbfd92c32827b21cc94
NataliS77 <107251503+NataliS77@users.noreply.github.com>
2022-10-20 15:25:13 +0300

    tasks 5 and 6  change

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,13 +1,16 @@
-const refs = {
-    inputText: document.querySelector('#name-input'),
-    spanText: document.querySelector('#name-output'),
-    
-};
 
-refs.inputText.addEventListener('input', onInputChange)
+const textInput = document.querySelector('#name-input');
+const textSpan = document.querySelector('#name-output');
 
-function onInputChange(event) {
-    console.log(event.currentTarget.value);
-    refs.spanText.textContent = event.currentTarget.value;
-    
-}
+
+
+textInput.addEventListener('input', () => {
+    if (textSpan.textContent = textInput.value) {
+       
+        
+    } else { textSpan.textContent = 'Anonymous'
+       
+        
+    }
+
+})
---
+++ src/js/task-06.js
@@ -0,0 +1,13 @@
+ const textInput = document.querySelector('#validation-input')
+
+
+textInput.addEventListener('blur',  () =>{
+    if (textInput.value.length == textInput.getAttribute('data-length')) {
+        textInput.classList.add('valid');
+        textInput.classList.remove('invalid');
+    } else {
+        textInput.classList.remove('valid');
+        textInput.classList.add('invalid');
+    }
+
+});
